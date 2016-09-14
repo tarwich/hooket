97794a8ce66520d50cc34d2f5c27e9caea4dc88b
Samuel Dillow <tarwich@gmail.com>
2016-09-14 07:58:20 -0500

    Fixed killer

--- run.sh
+++ run.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
+set -x
+
 NAME=foo
 REPO=github.com/tarwich/hooket
 
-# Get the hash of the current version
-CURRENT_HASH=$(git rev-parse HEAD)
 # Get any new data
 git pull
 # Get the hash of the new version
 NEW_HASH=$(git rev-parse HEAD)
 
 # If the hashes have changed then we're going to need to redeploy
-if [ "$CURRENT_HASH" != "$NEW_HASH" ] ; then
-  ID=$(docker ps -q --filter name=$NAME)
+if [ ! $(docker ps -qa --filter label=$NEW_HASH) ] ; then
+  # Kill the current image
+  ID=$(docker ps -qa --filter name=$NAME)
   if [ -n "$ID" ] ; then
-    docker kill $ID
+    docker stop $ID
+    docker rm $ID
   fi
 fi
 
 # Run hooket
-ID=$(docker ps -aq --filter name=$NAME)
+ID=$(docker ps -q --filter name=$NAME)
 
 if [ -z "$ID" ] ; then
   docker build --rm -t $NAME $REPO
-  docker run -d --name $NAME $NAME start
+  docker run -d --label rev=$NEW_HASH --name $NAME $NAME start
 fi
