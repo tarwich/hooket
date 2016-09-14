d563b6e6fd241289c74c4713176b4812d135881e
Samuel Dillow <tarwich@gmail.com>
2016-09-14 07:25:41 -0500

    Testing

---
+++ config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  environmentMap: {
+    PORT: 'server.port',
+  },
+  logs: {
+    log: 'info'
+  },
+  server: {
+    port: 3006
+  },
+};
--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 'use strict';
 
 // Libraries
-const minimist = require('minimist');
+const config       = require('config').loadConfig('.');
+const express      = require('express');
+const log          = require('log')('express');
+// const childLog     = require('log')('ssh');
+// const childProcess = require('child_process');
 
 // Variables
-const args = minimist(process.argv.slice(2), {
-  defaults: {
-    port: 8080,
-  }
+const app = express();
+
+// let child = childProcess.spawn('ssh', [
+//   '-i', 'id_rsa',
+//   'proxy@45.55.69.10',
+//   '-N',
+//   '-R', '8080:localhost:' + config.server.port]
+// );
+
+// child.stdout.on('data', data => childLog.info(data));
+// child.stderr.on('data', data => childLog.error(data));
+
+app.all('*', (request, response) => {
+  console.log(require('util').inspect(request, {colors: true}));
+  response.send('Hi there');
 });
 
-if (args._[0] === 'server') require('./server')(args);
-else if (args._[0] === 'client') require('./client')(args);
-
-else {
-  console.error(`
-  Usage: ${process.argv.slice(0, 2).join(' ')} [server | client]
-    server:   Specifies that this should be the tunnel server
-    --port:   Specify the port that the server should run on
-    client:   Specifies that this should be the tunnel client
-    --server: Specify the server that the client should connect to
-`
-  );
-}
+app.listen(config.server.port, () => {
+  log.info('Server is listening on port', config.server.port);
+});
